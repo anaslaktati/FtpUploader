2c98a268133722daebb89da857b0c1699b7a4efa
anaslaktati <anas.laktati@gmail.com>
2017-03-27 13:59:23 +0200

    commit

---
+++ ftpUploader/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ftpUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.treeViewUser = new System.Windows.Forms.TreeView();
+            this.treeViewServer = new System.Windows.Forms.TreeView();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblConnect = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(332, 73);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server :";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(133, 24);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(146, 20);
+            this.txtServer.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "UserName :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(133, 50);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(146, 20);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 76);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(146, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // treeViewUser
+            // 
+            this.treeViewUser.Location = new System.Drawing.Point(57, 160);
+            this.treeViewUser.Name = "treeViewUser";
+            this.treeViewUser.Size = new System.Drawing.Size(158, 142);
+            this.treeViewUser.TabIndex = 3;
+            // 
+            // treeViewServer
+            // 
+            this.treeViewServer.Location = new System.Drawing.Point(249, 160);
+            this.treeViewServer.Name = "treeViewServer";
+            this.treeViewServer.Size = new System.Drawing.Size(158, 142);
+            this.treeViewServer.TabIndex = 3;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(57, 111);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(350, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lblConnect
+            // 
+            this.lblConnect.AutoSize = true;
+            this.lblConnect.Location = new System.Drawing.Point(193, 137);
+            this.lblConnect.Name = "lblConnect";
+            this.lblConnect.Size = new System.Drawing.Size(77, 13);
+            this.lblConnect.TabIndex = 5;
+            this.lblConnect.Text = "connecting 0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 314);
+            this.Controls.Add(this.lblConnect);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.treeViewServer);
+            this.Controls.Add(this.treeViewUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.TreeView treeViewUser;
+        private System.Windows.Forms.TreeView treeViewServer;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblConnect;
+    }
+}
+
---
+++ ftpUploader/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ftpUploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct FtpSetting
+        {
+            public string server { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+            public string FileName { get; set; }
+            public string Fullname { get; set; }
+        }
+
+        FtpSetting inputParameter;
+
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string filename = ((FtpSetting)e.Argument).FileName;
+            string fullname = ((FtpSetting)e.Argument).Fullname;
+            string username = ((FtpSetting)e.Argument).username;
+            string password = ((FtpSetting)e.Argument).password;
+            string server = ((FtpSetting)e.Argument).server;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}",server,filename)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(username, password);
+            Stream ftpStream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(fullname);
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length ;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                if (!backgroundWorker.CancellationPending)
+                {
+                    byteRead = fs.Read(buffer, 0, 1024);
+                    ftpStream.Write(buffer, 0, byteRead);
+                    read += (double)byteRead;
+                    double pourcentage = read / total * 100;
+                    backgroundWorker.ReportProgress((int)pourcentage);
+                }
+
+            } while (byteRead != 0);
+            fs.Close();
+            ftpStream.Close();
+
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblConnect.Text = $"connecting {e.ProgressPercentage} %";
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblConnect.Text = "connection complete";
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd=new OpenFileDialog() { Multiselect=false,ValidateNames=true,Filter="All files|*.*" })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    inputParameter.username = txtUserName.Text;
+                    inputParameter.password = txtPassword.Text;
+                    inputParameter.server = txtServer.Text;
+                    inputParameter.FileName = fi.Name;
+                    inputParameter.Fullname = fi.FullName;
+
+                }
+            }
+        }
+    }
+}
